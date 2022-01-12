f4d072e696a04e757436a4ef124e47589f7b601c
Akinro Akintunde <aktakinro@gmail.com>
2022-01-12 04:23:27 -0800

    commit

--- user_control/views.py
+++ user_control/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import jwt
+from .models import jwt
+from .models import CustomUser
+from datetime import datetime, timedelta
+from django.conf import settings
+import random
+import string
+from rest_framework.views import APIView
+from .serializers import LoginSerializer, RefreshSerializer, RegisterSerializer
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from .authentication import Authentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+def get_random(length):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+def get_access_token(payload):
+    return jwt.encode(
+        {'exp': datetime.now() + timedelta(minutes=5), **payload},
+        settings.SECRET_KEY,
+        algorithm='HS256'
+    )
