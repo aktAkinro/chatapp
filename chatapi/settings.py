ff581bd190b13e893421bdcd1369748013ef576b
Akinro Akintunde <aktakinro@gmail.com>
2022-01-12 01:02:31 -0800

    getting started

--- chatapi/settings.py
+++ chatapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y=%did64^nn+bsprz1^cpwqd+ah4etifp9mx)%g3_w2u=xph6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'chatapi',
+    'user_control',
 ]
 
 MIDDLEWARE = [
